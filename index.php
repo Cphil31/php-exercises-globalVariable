97831de1e8972e29cff94f0d675d2c64ccef0d35
Cphil31 <cecile_philippe@live.fr>
2017-01-12 12:22:05 +0100

    ajout du formulaire user et login

--- index.php
+++ index.php
@@ -7,29 +7,44 @@
 </head>
 <body>
 	<div class="menu" >
-	<a href="contenu.php">Contenu</a>
+		<a href="contenu.php">Contenu</a>
 		
 	</div>
-<div class="phrase">
-	
-	<?php 
+	<div class="phrase">
 
-echo "Bonjour ".$_POST['genre']." ".$_POST['Nom']." ".$_POST['Prenom']." votre user agent est : ".$_SERVER['HTTP_USER_AGENT'];
+		<?php 
+		include ('style.php');
+		echo "Bonjour ".$_POST['genre']." ".$_POST['Nom']." ".$_POST['Prenom']." votre user agent est : ".$_SERVER['HTTP_USER_AGENT'];
 
-?>
-</div>
-<div class="phrase">
-<?php 
-echo "Votre adresse Ip est : ".$_SERVER["REMOTE_ADDR"];
- ?>	
-</div>
-<div class="phrase">
-<?php 
-	echo " Le nom du serveur est : ".$_SERVER['DOCUMENT_ROOT'];
- ?>
+		?>
+	</div>
+	<div class="phrase">
+		<?php 
+		echo "Votre adresse Ip est : ".$_SERVER["REMOTE_ADDR"];
+		?>	
+	</div>
+	<div class="phrase">
+		<?php 
+		echo " Le nom du serveur est : ".$_SERVER['DOCUMENT_ROOT'];
+		?>
 
-</div>
+	</div>
+
+	<form action="index.php" method="POST">
+		<div>
+		<label for="">User : </label>
+			<input type="text" name="user">
+		</div>
+		<div>
+			<label for="">login : </label>
+			<input type="text" name ="login">
+		</div>
+		<div>
+			<input type="submit" name="submit" value="Valider">
+		</div>
+
+	</form>
 
 
-			</body>
-			</html>
+</body>
+</html>
