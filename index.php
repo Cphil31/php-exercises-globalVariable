4e928849c9ac9e9034777d48e1b691c623e0ae43
Cphil31 <cecile_philippe@live.fr>
2017-01-12 11:01:43 +0100

    step 1 fait

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+
+	<form action="user.php" method="POST">
+
+		<SELECT name="genre" >
+			<OPTION>Mr
+			<OPTION>Mme
+			<OPTION>Mlle
+		</SELECT>
+
+		<label for="">Nom</label>
+		<input type="text" name="Nom">
+		<label for="">Prénom </label>
+		<input type="text" name ="Prenom">
+		<input type="submit" name="submit" value="Valider">
+
+	</form> 
+
+
+			</body>
+			</html>
---
+++ user.php
@@ -0,0 +1,4 @@
+<?php 
+
+echo "Bonjour ".$_POST['genre']." ".$_POST['Nom']." ".$_POST['Prenom']." votre adresse Ip est ".$_SERVER["REMOTE_ADDR"];;
+ ?>
