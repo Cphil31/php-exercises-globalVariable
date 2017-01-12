ce06d4693340910c9518b7634df66b18eba22c80
Cphil31 <cecile_philippe@live.fr>
2017-01-12 11:32:01 +0100

    correction step 1

--- user.php
+++ user.php
@@ -1,6 +1,6 @@
 <?php 
 
-echo "Bonjour ".$_POST['genre']." ".$_POST['Nom']." ".$_POST['Prenom']." votre user agent est ".$_SERVER['HTTP_USER_AGENT']." et votre adresse Ip est ".$_SERVER["REMOTE_ADDR"]." le nom du serveur est ".$_SERVER['DOCUMENT_ROOT'];
+echo "Bonjour ".$_POST['genre']." ".$_POST['Nom']." ".$_POST['Prenom']." votre user agent est : ".$_SERVER['HTTP_USER_AGENT']." et votre adresse Ip est : ".$_SERVER["REMOTE_ADDR"]." le nom du serveur est : ".$_SERVER['DOCUMENT_ROOT'];
 
 
 
